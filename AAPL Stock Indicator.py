2b6d62d165229c44c1812dea70828eec6102f427
sankarrevath <sankarrevath@gmail.com>
2019-01-10 22:18:34 -0600

    AAPL Stock Indicator with Visualization

--- AAPL Stock Indicator.py
+++ AAPL Stock Indicator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import datetime
 import quandl
+import matplotlib.pyplot as plt
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -9,26 +9,16 @@ from sklearn.linear_model import LinearRegression
 
 pd.set_option('display.expand_frame_repr', False)#allows wrap around if num cols exceeds max_cols
 
-import getpass
-USER_NAME = getpass.getuser()
-
-
-def add_to_startup(file_path=""):
-    if file_path == "":
-        file_path = os.path.dirname(os.path.realpath(__file__))
-    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-        bat_file.write(r'start "" %s' % file_path)
-
-
-df = quandl.get('WIKI/AAPL')
+#create a APIConfig Key online free with quandl and use command -> quandl.ApiConfig.api_key = 'key_str'
+quandl.ApiConfig.api_key = 'FU4P32QPSN87TtrNaxML'
+df = quandl.get('WIKI/AMZN')
 df = df[['Adj. Close']]
 
 forecast_out = int(30)
 df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
 
 X = np.array(df.drop(['Prediction'],1))
-X = preprocessing.scale(X)
+X = preprocessing.scale(X) # used to standardize a dataset along any axis
 
 X_forecast = X[-forecast_out:]
 X = X[:-forecast_out]
@@ -38,11 +28,20 @@ y = y[:-forecast_out]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-clf = LinearRegression()
-clf.fit(X_train, y_train)
+clf = LinearRegression() # implement attributes of the LinearRegression object
+clf.fit(X_train, y_train)  # standard fit function with the y_train sample weight being set to 1
 
-confidence = clf.score(X_test, y_test)
-print('confidence: ', confidence)
+confidence = clf.score(X_test, y_test)# score function takes into account natural accuracy as well as the time for the prediction to be given to
+print('confidence: ', confidence)#prints the confidence score onto the terminal, using formula -> (correct predictions / total predictions) * 100%
 
 forecast_prediction = clf.predict(X_forecast)
 print(forecast_prediction)
+
+#line graph prediction
+
+days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+#new matplot addon allows for visualization of 30 day projected stock trend as a line graph
+plt.plot(days,forecast_prediction,color='b')#input one is the x_axis, number two is the y_axis and the final one is the color
+plt.xlabel('Days from Today')#label x axis as 'Days from Today'
+plt.ylabel('Forecasted Stock Projection')#label y axis as 'Forecasted Stock Projection'
+plt.show()#displays the visualization on the users device
